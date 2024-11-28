c7561934ab8d94d86a497f101d81370e26882564
acharayaP03 <acharyaP03@gmail.com>
2024-11-28 14:28:45 +1100

    initial navigation for app

---
+++ src/layouts/Navigation.jsx
@@ -0,0 +1,30 @@
+import { hamburger } from '../assets/icons';
+import { headerLogo } from '../assets/images';
+import { navigationLinks } from '../constants';
+
+export default function Navigation() {
+  return (
+    <header className="padding-x absolute z-10 w-full py-8">
+      <nav className="max-container flex items-center justify-between">
+        <a href="/">
+          <img src={headerLogo} alt="Logo" width={130} height={29} />
+        </a>
+        <ul className="flex flex-1 items-center justify-center gap-16 max-lg:hidden">
+          {navigationLinks.map((link) => (
+            <li key={link.label}>
+              <a
+                href={link.href}
+                className="font-montserrat text-slate-gray text-lg leading-normal"
+              >
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="Hamburger menu" width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  );
+}
