73beb6c770ad7dbae232fbded49c3c88473b6126
acharayaP03 <acharyaP03@gmail.com>
2024-11-28 14:27:52 +1100

    use all feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
+import {
+  CustomerReviews,
+  Hero,
+  SuperQuality,
+  SpecialOffers,
+  PopularProduct,
+  Services,
+  Subscribe,
+} from './features';
+
+import Footer from './layouts/Footer';
+import Navigation from './layouts/Navigation';
+
 export default function App() {
   return (
     <main className="relative">
-      <nav></nav>
-      <section className="xs:padding-1 wide:padding-r padding-b">Hero</section>
-      <section className="padding">Popular product section</section>
-      <section className="padding">Super quality</section>
-      <section className="padding">Services</section>
-      <section className="padding">Special offers</section>
-      <section className="padding bg-pale-blue">Customer reviews</section>
-      <section className="padding-x w-full py-16 sm:py-32">Subscrive</section>
-      <section className="padding-x padding-t bg-black pb-8">footer</section>
+      <Navigation />
+      <Hero />
+      <PopularProduct />
+      <SuperQuality />
+      <Services />
+      <SpecialOffers />
+      <CustomerReviews />
+      <Subscribe />
+      <Footer />
     </main>
   );
 }
