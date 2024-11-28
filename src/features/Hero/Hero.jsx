9d410bc43b7eae3cc4758f15a2e887ef5a6c2be5
acharayaP03 <acharyaP03@gmail.com>
2024-11-28 22:07:52 +1100

    break hero into lean and clean

--- src/features/Hero/Hero.jsx
+++ src/features/Hero/Hero.jsx
@@ -1,37 +1,15 @@
-import { arrowRight } from '../../assets/icons';
+import HeroCta from './HeroCta';
+import HeroFeaturedProduct from './HeroFeaturedProduct';
 
-import Statistics from '../../components/HeroSection/Statistics';
-import { Button, Paragraph } from '../../ui';
 const Hero = () => {
   return (
-    <section
+    <div
       id="home"
-      className="max-container xs:padding-1 wide:padding-r padding-b flex min-h-screen w-full flex-col justify-center gap-10 xl:flex-row"
+      className="max-container flex min-h-screen w-full flex-col justify-center gap-10 xl:flex-row"
     >
-      <div className="xl:2/5 max-xl:padding-x relative flex w-full flex-col items-start justify-center pt-28">
-        <Paragraph
-          textStyle="font-montserrat text-coral-red text-xl"
-          text="Our summar collection"
-        />
-        <h1 className="font-palanquin mt-10 text-8xl font-bold max-sm:text-[72px] max-sm:leading-[82]">
-          <span className="xl-whitespace-nowrap relative z-10 pr-10 xl:bg-white">
-            The New Arrival
-          </span>
-          <br />
-          <span className="text-coral-red mt-3 inline-block">
-            Nike
-          </span> Shoes
-        </h1>
-
-        <Paragraph
-          textStyle="font-montserrat text-slate-gray mb-14 mt-6 text-lg leading-8 sm:max-w-sm"
-          text="Discover stylish Nike arrivals, quality comfort, and innovation for
-          your active life."
-        />
-        <Button label="Shop now" iconUrl={arrowRight} />
-        <Statistics />
-      </div>
-    </section>
+      <HeroCta />
+      <HeroFeaturedProduct />
+    </div>
   );
 };
 
---
+++ src/features/Hero/HeroCta.jsx
@@ -0,0 +1,31 @@
+import { arrowRight } from '../../assets/icons';
+import Statistics from '../../components/HeroSection/Statistics';
+import { Button, Paragraph } from '../../ui';
+
+export default function HeroCta() {
+  return (
+    <div className="max-xl:padding-x relative flex w-full flex-col items-start justify-center pt-28 xl:w-2/5">
+      <Paragraph
+        textStyle="font-montserrat text-coral-red text-xl"
+        text="Our summar collection"
+      />
+      <h1 className="font-palanquin mt-10 text-8xl font-bold max-sm:text-[72px] max-sm:leading-[82px]">
+        <span className="relative z-10 pr-10 xl:whitespace-nowrap xl:bg-white">
+          The New Arrival
+        </span>
+        <br />
+        <span className="text-coral-red mt-3 inline-block">
+          Nike
+        </span> Shoes
+      </h1>
+
+      <Paragraph
+        textStyle="font-montserrat text-slate-gray mb-14 mt-6 text-lg leading-8 sm:max-w-sm"
+        text="Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life."
+      />
+      <Button label="Shop now" iconUrl={arrowRight} />
+      <Statistics />
+    </div>
+  );
+}
