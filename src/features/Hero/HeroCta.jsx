4e6f3da97a2c225eb96f2f962fc513def0db0a4a
acharayaP03 <acharyaP03@gmail.com>
2024-11-29 07:55:13 +1100

    first component test

--- src/features/Hero/HeroCta.jsx
+++ src/features/Hero/HeroCta.jsx
@@ -1,6 +1,6 @@
-import { arrowRight } from '../../assets/icons';
-import Statistics from '../../components/HeroSection/Statistics';
-import { Button, Paragraph } from '../../ui';
+import { arrowRight } from '@/assets/icons';
+import Statistics from '@/components/HeroSection/Statistics';
+import { Button, Paragraph } from '@/ui';
 
 export default function HeroCta() {
   return (
@@ -9,12 +9,12 @@ export default function HeroCta() {
         textStyle="font-montserrat text-coral-red text-xl"
         text="Our summar collection"
       />
-      <h1 className="font-palanquin mt-10 text-8xl font-bold max-sm:text-[72px] max-sm:leading-[82px]">
+      <h1 className="mt-10 font-palanquin text-8xl font-bold max-sm:text-[72px] max-sm:leading-[82px]">
         <span className="relative z-10 pr-10 xl:whitespace-nowrap xl:bg-white">
           The New Arrival
         </span>
         <br />
-        <span className="text-coral-red mt-3 inline-block">
+        <span className="mt-3 inline-block text-coral-red">
           Nike
         </span> Shoes
       </h1>
