f3a01229a55ed3d309a8275902b97dac7e47050b
acharayaP03 <acharyaP03@gmail.com>
2024-11-28 16:00:26 +1100

    initial hero section

--- src/features/Hero/Hero.jsx
+++ src/features/Hero/Hero.jsx
@@ -1,8 +1,43 @@
-import React from 'react';
-
+import Button from '../../ui/Button';
+import { arrowRight } from '../../assets/icons';
+import { statistics } from '../../constants';
 const Hero = () => {
   return (
-    <section className="xs:padding-1 wide:padding-r padding-b">Hero</section>
+    <section
+      id="home"
+      className="max-container xs:padding-1 wide:padding-r padding-b flex min-h-screen w-full flex-col justify-center gap-10 xl:flex-row"
+    >
+      <div className="xl:2/5 max-xl:padding-x relative flex w-full flex-col items-start justify-center pt-28">
+        <p className="font-montserrat text-coral-red text-xl">
+          Our summar collection
+        </p>
+        <h1 className="font-palanquin mt-10 text-8xl font-bold max-sm:text-[72px] max-sm:leading-[82]">
+          <span className="xl-whitespace-nowrap relative z-10 pr-10 xl:bg-white">
+            The New Arrival
+          </span>
+          <br />
+          <span className="text-coral-red mt-3 inline-block">
+            Nike
+          </span> Shoes
+        </h1>
+        <p className="font-montserrat text-slate-gray mb-14 mt-6 text-lg leading-8 sm:max-w-sm">
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+        <Button label="Shop now" iconUrl={arrowRight} />
+
+        <div className="mt-20 flex w-full flex-wrap items-start justify-start gap-16">
+          {statistics.map((stat) => (
+            <div key={stat.label}>
+              <p className="font-palanquin text-4xl font-bold">{stat.value}</p>
+              <p className="font-montserrat text-slate-gray leading-7">
+                {stat.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 };
 
